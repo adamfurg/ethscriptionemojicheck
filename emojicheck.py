6f0352106775d6e25644a08187a7240bf2100f80
adamfurg <135074677+adamfurg@users.noreply.github.com>
2023-07-17 20:26:07 -0400

    Update emojicheck.py

--- emojicheck.py
+++ emojicheck.py
@@ -1,4 +1,5 @@
 import requests
+import streamlit as st
 
 # Function to check if a SHA exists
 def check_sha_exists(sha):
@@ -18,16 +19,17 @@ with open("hash good eth.txt", "r") as file:
 emoji_to_hash = dict(zip(emojis, hashes))
 
 # Get the emoji from the user
-emoji = input("Please enter the emoji you want to check: ")
+emoji = st.text_input("Please enter the emoji you want to check: ")
 
-sha = emoji_to_hash.get(emoji)  # Returns None if the emoji is not in the dictionary
+if emoji:  # Only run the rest of the code if the user has entered an emoji
+    sha = emoji_to_hash.get(emoji)  # Returns None if the emoji is not in the dictionary
 
-if sha is not None:
-    response_data = check_sha_exists(sha)
-    if response_data['result']:
-        print(f"Result: {response_data['result']}")
-        print(f"Current owner: {response_data['ethscription']['current_owner']}")
+    if sha is not None:
+        response_data = check_sha_exists(sha)
+        if response_data['result']:
+            st.write(f"Result: {response_data['result']}")
+            st.write(f"Current owner: {response_data['ethscription']['current_owner']}")
+        else:
+            st.write(f"Result: {response_data['result']}")
     else:
-        print(f"Result: {response_data['result']}")
-else:
-    print(f"No hash found for emoji: {emoji}")
+        st.write(f"No hash found for emoji: {emoji}")
